615e42bcdc8385a67506de97cf854be200e58373
Tony Theodoropoulos <theodot@outlook.com>
2020-03-05 14:53:05 -0500

    Add project files.

---
+++ Hurricane/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Hurricane
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.categoryInput = new System.Windows.Forms.TextBox();
+            this.enterButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // categoryInput
+            // 
+            this.categoryInput.Location = new System.Drawing.Point(185, 111);
+            this.categoryInput.Name = "categoryInput";
+            this.categoryInput.Size = new System.Drawing.Size(100, 20);
+            this.categoryInput.TabIndex = 0;
+            // 
+            // enterButton
+            // 
+            this.enterButton.Location = new System.Drawing.Point(204, 175);
+            this.enterButton.Name = "enterButton";
+            this.enterButton.Size = new System.Drawing.Size(75, 23);
+            this.enterButton.TabIndex = 1;
+            this.enterButton.Text = "Enter";
+            this.enterButton.UseVisualStyleBackColor = true;
+            this.enterButton.Click += new System.EventHandler(this.enterButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(110, 270);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(35, 13);
+            this.outputLabel.TabIndex = 2;
+            this.outputLabel.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 450);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.enterButton);
+            this.Controls.Add(this.categoryInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox categoryInput;
+        private System.Windows.Forms.Button enterButton;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ Hurricane/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hurricane
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void enterButton_Click(object sender, EventArgs e)
+        {
+            //get inputs
+            string category = categoryInput.Text;
+
+            //process the input
+            switch (category)
+            {
+                case "1":
+                    outputLabel.Text = "119-153 km/hr";
+                    break;
+                case "2":
+                    outputLabel.Text = "154-177 km/hr";
+                    break;
+                case "3":
+                    outputLabel.Text = "178-209 km/hr";
+                    break;
+                case "4":
+                    outputLabel.Text = "210-249 km/hr";
+                    break;
+                case "5":
+                    outputLabel.Text = "greater than 249 km/hr";
+                    break;
+                default:
+                    outputLabel.Text = "That's not a valid category";
+                    break;
+            }
+        }
+    }
+}
